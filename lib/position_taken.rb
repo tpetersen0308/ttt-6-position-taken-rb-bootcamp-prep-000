c9105d4377385a1830d6e1690acfbabab6faf023
Travis Petersen <travis.e.petersen@gmail.com>
2017-10-05 18:35:15 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,7 +1,8 @@
 # code your #position_taken? method here!
 def position_taken?(board, index)
-  if board[index] != "X" || board[index] != "O"
+  if board[index] == "X" || board[index] == "O"
+    true
+  else 
     false
-  else true
   end
 end
